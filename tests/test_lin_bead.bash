2da04022d04cb00726adf9cdb131bf350bcddda8
spiridon.laurentiu@gmail.com <spiridon.laurentiu@gmail.com>
2016-11-18 07:35:02 -0600

    Shrinked lin_bead.in

--- tests/test_lin_bead.bash
+++ tests/test_lin_bead.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-python ../lin_bead.py --dir ./ --inFNRoots  lin_bead.in --skiprows 300 --nbins 50 --xmin -180 --xmax 180 --makehist --makeplot
+python ../lin_bead.py --dir ./ --inFNRoots  lin_bead.in --skiprows 30 --nbins 50 --xmin -180 --xmax 180 --makehist --makeplot
 if [ "$?" -eq 0 ] 
 then
   echo "Test 0 for lin_bead.py passed."
